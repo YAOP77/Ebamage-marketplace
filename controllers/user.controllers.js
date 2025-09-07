d0bfe4a1db8517b91c2306be518881a234151e3a
YAOP77 <yaop77@gmail.com>
2025-09-07 09:06:20 +0000

    ajoutons

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -36,7 +36,6 @@ const RegisterUser = async (req, res) => {
             deviceToken: deviceToken || null,
             otpExpiresAt: new Date(Date.now() + 5 * 60 * 1000), // expire dans 5 min
             role: "user",
-            type: userType,
             // codeOtp: otpCode
             ...(process.env.NODE_ENV !== "production" && { codeOtp: otpCode } )
         }
